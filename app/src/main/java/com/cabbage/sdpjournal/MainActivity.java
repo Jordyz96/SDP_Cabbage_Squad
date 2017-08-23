3e43e7ebe0e0c30c9ab5ae8b45dc9029a409157f
Seanny Cow <sean.carmichael2@gmail.com>
2017-08-23 23:54:25 +1000

    Login & Registration
    Created a login & registration function using Firebase

---
+++ app/src/main/java/com/cabbage/sdpjournal/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cabbage.sdpjournal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
